67049f8eeeceafae893cffabfc059b5f6071603d
DY <aaskorohodov@gmail.com>
2024-01-26 16:07:04 +0500

    Minor

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from fastapi import FastAPI
 from news_app.main import news_app
 from news_app.utils.env_setter import EnvSetter
 
-# Create an instance of FastAPI
+
+# Строим инстанс приложения и подключаем роутеры
 root_app = FastAPI()
-
-
-# Include the FastAPI app from the app directory
 root_app.include_router(news_app.router)
 
+# Для запуска в режиме разработки
 if __name__ == "__main__":
     import uvicorn
 
+    # Читаем переменные окружения (пути к файлам-json)
     EnvSetter.set_envs(__file__)
 
-    # Run the FastAPI app at the root level
     uvicorn.run(root_app, host="127.0.0.1", port=8000)
--- news_app/main.py
+++ news_app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 
+# Инстанс для основного (единственного) приложения
 news_app = FastAPI()
 
 from news_app.endpoints.all_news import all_news
 from news_app.endpoints.news_by_id import news_by_id
 
+# Подключаем роутеры
 news_app.include_router(all_news.router)
 news_app.include_router(news_by_id.router)
